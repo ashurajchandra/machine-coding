15760587a72ce7d71f41038ef4a5adab7a378ee2
ashurajchandra <mukutrai12@gmail.com>
2025-02-16 20:31:20 +0530

    refactored star rating code

--- app/modules/starRating.tsx
+++ app/modules/starRating.tsx
@@ -1,78 +1,45 @@
+"use client";
+import { useState, useMemo } from "react";
 
-"use client"
-import {useState, useEffect} from 'react';
-
-interface Star{
-    id:number;
-    url:string;
-    active:boolean;
+interface Props {
+  stars: number;
+  activeStars: number;
 }
-interface Props{
-    stars: number;
-    activeStars: number
-}
-const empty_star = 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Empty_Star.svg';
-const filled_star = 'https://upload.wikimedia.org/wikipedia/commons/4/44/Plain_Yellow_Star.png';
+const empty_star =
+  "https://upload.wikimedia.org/wikipedia/commons/e/e7/Empty_Star.svg";
+const filled_star =
+  "https://upload.wikimedia.org/wikipedia/commons/4/44/Plain_Yellow_Star.png";
 
-const StarRating = (props:Props) =>{
-    const {stars, activeStars} = props;
-    const [currentActiveStars, setCurrentActiveStars] = useState<Star[]>([]);
-    const [prevStateData, setPrevStateData] = useState<Star[]>([]);
-    const [isClicked, setIsClicked] = useState(false)
+const StarRating = (props: Props) => {
+  const { stars, activeStars } = props;
 
-    useEffect(()=>{
-    const modifiedStars=  Array.from({length:stars}).map((_,index)=>({id:index, url:empty_star,active:false}));
+  const [selectedRating, setSelectedRating] = useState<number>(activeStars);
+  const [hoveredRating, setHoveredRating] = useState<number | null>(null);
 
-    Array.from({length:activeStars}).forEach((_,index)=>{
-        modifiedStars[index].url = filled_star;
-        modifiedStars[index].active = true  
-    })
-    setCurrentActiveStars(modifiedStars)
-    setPrevStateData(modifiedStars)
-    },[])
+  const starsArray = useMemo(() => {
+    return Array.from({ length: stars }).map((_, index) => index + 1);
+  }, [stars]);
 
-    console.log("currentActiveStars",currentActiveStars)
+  const handleClick = (currentRating: number) => {
+    setSelectedRating(currentRating);
+  };
 
-    
+  const displayRating = hoveredRating !== null ? hoveredRating : selectedRating;
 
-    const updateStars = (id:number, isFinal:boolean) =>{
-        const localStars = currentActiveStars.map((star,index)=>(
-            {
-                ...star,
-                url: index<=id? filled_star: empty_star,
-                active: index<=id
-            }
-        ))
-        setCurrentActiveStars(localStars)
-
-        if(isFinal){
-            setIsClicked(true)
-            setPrevStateData(localStars)
-        }
-    }
-    const handleMouseLeave = () =>{
-        if(!isClicked){
-            setCurrentActiveStars(prevStateData)
-        }
-        setIsClicked(false)
-    }
-
-    return (
-        <div className='flex gap-[3px]'
-        >
-            {
-             currentActiveStars.length>0 && currentActiveStars.map(({id,url,active},index)=>(
-                  <img 
-                  src={url} 
-                  onClick={()=>updateStars(id, true)} 
-                  className="w-[30px] h-[30px]" key={id} 
-                  onMouseEnter={()=>updateStars(id, false)}
-                  onMouseLeave = {handleMouseLeave}
-                  />
-             )) 
-            }
-        </div>
-    )
-}
+  return (
+    <div className='flex gap-[3px]'>
+      {starsArray.map((starRating, index) => (
+        <img
+          key={index}
+          className='w-[30px] h-[30px]'
+          onClick={() => handleClick(starRating)}
+          onMouseEnter={() => setHoveredRating(starRating)}
+          onMouseLeave={() => setHoveredRating(null)}
+          src={starRating <= displayRating ? filled_star : empty_star}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default StarRating;
